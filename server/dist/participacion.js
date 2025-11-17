7c0d248bb780fb5260cb1e8e62d6474c512e6320
danielmarhuenda <alu0101487731@ull.edu.es>
2025-11-17 15:39:47 +0000

    chore: untrack server/dist

--- server/dist/participacion.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Ejemplo de documento:
-// {
-//   "id_participacion": "c9d4f1e2-...",
-//   "id_usuario": "5f8d0d55b54764421b7156c4",
-//   "id_gasto": "d3e5f6a7-...",
-//   "monto_asignado": 50.75
-// }
